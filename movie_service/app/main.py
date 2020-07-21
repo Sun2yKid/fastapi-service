acb1ce1b54185992fa469ed04fba3af09b3c5d85
zhonghui.yao <zhonghua00700@gmail.com>
2020-07-21 15:46:56 +0800

    update

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
+from app.api.movies import movies
+
 app = FastAPI()
 
 app.include_router(movies)
